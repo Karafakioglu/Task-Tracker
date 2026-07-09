3d7e18d57bc8c5809b6390484b8dcce1bbb5312c
Ismail Karafakioglu <ismailkarafakioglu@gmail.com>
2026-07-09 20:02:23 +0200

    filled in add command

--- task_tracker.py
+++ task_tracker.py
@@ -1,65 +1,107 @@
 import sys
+import json
+import os
+import datetime
 
 if len(sys.argv) < 2:
     sys.exit("Enter a value to make changes in tasks")
 
-command = sys.argv[1]
-rest = sys.argv[2: ]
+COMMAND = sys.argv[1]
+REST = sys.argv[2: ]
+
+def load_tasks():
+    if os.path.exists("task_list.json"):
+        with open("task_list.json", "r") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
+    else:
+        with open("task_list.json", mode="w") as write_file:
+            json.dump([], write_file)
+        return []
 
 
-if command == "add":
-    if len(rest) != 1:
+def save_tasks(tasks):
+    with open("task_list.json", "w") as f:
+        json.dump(tasks, f)
+
+tasks = load_tasks()
+
+def get_next_id(tasks):
+    temp_list = []
+    for task in tasks:
+        temp_list.append(task["id"])
+    
+    try:
+        return max(temp_list) + 1
+    except ValueError:
+        return 1
+
+    
+
+
+
+if COMMAND == "add":
+    current_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    if len(REST) != 1:
         sys.exit("Enter task values (minimum 1)")
     else:
-        print(f"{rest} is added")
+        new_dict = {"id": get_next_id(tasks), "description": REST[0], "status": "todo", "CreatedAt": current_time, "UpdatedAt": current_time}
+        tasks.append(new_dict)
+        save_tasks(tasks)
 
-elif command == "update":
-    if len(rest) != 2:
+
+        print(f"{REST} is added")
+
+
+elif COMMAND == "update":
+    if len(REST) != 2:
         sys.exit("Enter task values")
     else:
         try:
-            task_id = int(rest[0])
+            task_id = int(REST[0])
         except ValueError:
             sys.exit("Task ID must be a number")
-        print(f"updated {rest}")
+        print(f"updated {REST}")
 
-elif command == "delete":
-    if len(rest) != 1:
+elif COMMAND == "delete":
+    if len(REST) != 1:
         sys.exit("Enter task values")
     else:
         try:
-            task_id = int(rest[0])
+            task_id = int(REST[0])
         except ValueError:
             sys.exit("Task ID must be a number")
-        print(f"placeholder: {rest}")
+        print(f"placeholder: {REST}")
 
-elif command == "mark-in-progress":
-    if len(rest) != 1:
+elif COMMAND == "mark-in-progress":
+    if len(REST) != 1:
         sys.exit("Enter task values")
     else:
         try:
-            task_id = int(rest[0])
+            task_id = int(REST[0])
         except ValueError:
             sys.exit("Task ID must be a number")
-        print(f"The following are the tasks: {rest}")
+        print(f"The following are the tasks: {REST}")
 
-elif command == "mark-done":
-    if len(rest) != 1:
+elif COMMAND == "mark-done":
+    if len(REST) != 1:
         sys.exit("Enter task values")
     else:
         try:
-            task_id = int(rest[0])
+            task_id = int(REST[0])
         except ValueError:
             sys.exit("Task ID must be a number")
-        print(f"The following are the tasks: {rest}")
+        print(f"The following are the tasks: {REST}")
 
-elif command == "list":
-    if len(rest) > 1:
-        print(len(rest))
+elif COMMAND == "list":
+    if len(REST) > 1:
         sys.exit("Enter task values")
     else:
-        print(f"The following are the tasks: {rest}")
-        print(len(rest))
+        print(f"The following are the tasks: {REST}")
 
 else:
-    sys.exit(f"Unkown command: {command}")
+    sys.exit(f"Unkown COMMAND: {COMMAND}")
+
+
